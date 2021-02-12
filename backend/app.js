63776940e91e059430dc29daff350a0e3f3b94dc
Toni Pekkala <tonipekkala90@gmail.com>
2021-02-12 17:07:27 +0200

    joko

--- backend/app.js
+++ backend/app.js
@@ -6,26 +6,26 @@ app.use(express.json());
 app.use(express.static('build'))
 app.use(cors())
 
-// let notes = [
-//     {
-//       id: 1,
-//       content: "HTML is easy",
-//       date: "2019-05-30T17:30:31.098Z",
-//       important: true
-//     },
-//     {
-//       id: 2,
-//       content: "Browser can execute only Javascript",
-//       date: "2019-05-30T18:39:34.091Z",
-//       important: false
-//     },
-//     {
-//       id: 3,
-//       content: "GET and POST are the most important methods of HTTP protocol",
-//       date: "2019-05-30T19:20:14.298Z",
-//       important: true
-//     }
-//   ]
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      date: "2019-05-30T17:30:31.098Z",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only Javascript",
+      date: "2019-05-30T18:39:34.091Z",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      date: "2019-05-30T19:20:14.298Z",
+      important: true
+    }
+  ]
 
 const generateId = () => {
     const maxId = notes.length > 0
